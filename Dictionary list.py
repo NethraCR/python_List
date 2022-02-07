47093e04f0fc9ceef5bb0ed387b8a50ec2b49df9
NethraCR <98078370+NethraCR@users.noreply.github.com>
2022-02-07 17:04:07 +0530

    Add files via upload

---
+++ Dictionary list.py
@@ -0,0 +1,8 @@
+dict={ }
+dict['one']="This is one"
+dict[2]="This is two"
+tinydict={'name=Nani','code:4567','dept:ISE'}
+print(dict['one'])
+print(dict[2])
+print(tinydict)
+print(tinydict.keys())
