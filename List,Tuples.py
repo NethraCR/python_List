dfa225b810e65ce4498d97edf67d86c41e643628
NethraCR <98078370+NethraCR@users.noreply.github.com>
2022-02-07 17:04:46 +0530

    Add files via upload

---
+++ List,Tuples.py
@@ -0,0 +1,16 @@
+list=['ábcd',678,'john',70.2]
+tinylist=[123,'peter']
+print(list)
+print(list[0])
+print(list[1:3])
+print(tinylist*3)
+print(list+tinylist)
+
+#Tuples
+
+tuple=('abcd',765,2.33,'john',23)
+tinytuple=(123,'nani')
+print(tuple)
+print(tuple[0])
+print(tuple[1:3])
+
